5689c57f598edd91446028c81f63f3c77c9697bf
Vaishnavi <vaishnavishah349@gmail.com>
2024-12-29 13:36:56 +0530

    Add files via upload

---
+++ create_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+n = np.array((1,2,3)) # using tuple
+print(n)
+print(type(n))
+
+q= np.array([1,2,3,4]) # using list
+print(q)
+print(type(q))
---
+++ initialize_array.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# example 1:
+n = np.zeros([2,3]) # 2 rows and 3 columns
+print(n)
+print(type(n))
+
+# example 2:
+q = np.zeros([4,4]) # 4 rows and 4 columns
+print(q)
+print(type(q))
